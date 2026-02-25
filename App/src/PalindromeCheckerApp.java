8f80b433f74fed88240ecd8e7105f70c6933817d
as7395 <as7395@srmist.edu.in>
2026-02-25 10:08:59 +0530

    Update PalindromeCheckerApp.java

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -24,20 +24,3 @@ public class PalindromeCheckerApp {
             System.out.println(isPalindrome(input));  // true
         }
     }
-    public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter a word: ");
-            String input = scanner.nextLine();
-            input = input.replaceAll("\\s+", "").toLowerCase();
-            String reversed = "";
-            for (int i = input.length() - 1; i >= 0; i--) {
-                reversed += input.charAt(i);
-            }
-            if (input.equals(reversed)) {
-                System.out.println("The input is a palindrome.");
-            } else {
-                System.out.println("The input is NOT a palindrome.");
-            }
-            scanner.close();
-        }
-    }
